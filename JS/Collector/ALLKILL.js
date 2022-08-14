b7742d6293ed3e07c66632d178fb0a5d69462fcd
tpzscoreboardSupport <110368904+tpzscoreboardSupport@users.noreply.github.com>
2022-08-14 16:52:30 +0900

    TPZ 업데이트
    칭호 업데이트

---
+++ JS/Collector/ALLKILL.js
@@ -0,0 +1,15 @@
+let colors = ["black", "red", "blue" ,"green", "Yellow", "lime", "hotpink","orange"]
+let SaveCollect = document.getElementById('collector');
+SaveCollect.innerText = "[ALLKILL] ";
+SaveCollect.style.fontSize = "40px";
+let idx = 0;
+
+setInterval (function () {
+    SaveCollect.style.color = colors[idx];
+    idx++;
+    if (idx === colors.length){
+        idx = 0;
+    }
+},80);
+
+
